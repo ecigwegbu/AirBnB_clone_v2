71469dce8d6303cf13a6a7f7aaa019fac39c5dd8
jexjatto <jextrim@gmail.com>
2023-03-25 09:20:30 +0100

    airbnb done from vs

---
+++ models/engine/db_storage.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+"""New class for sqlAlchemy """
+from os import getenv
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import Base, BaseModel
+from models.state import State
+from models.city import City
+from models.user import User
+from models.place import Place
+from models.review import Review
+from models.amenity import Amenity
+
+
+class DBStorage:
+    """ create tables in environmental"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize a new instance of the database storage."""
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db),
+                                      pool_pre_ping=True)
+
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """returns a dictionary
+        Return:
+            returns a dictionary of __object
+        """
+        if cls is None:
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        dict = {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
+        return dict
+
+    def new(self, obj):
+        """add a new element in the table
+        """
+        self.__session.add(obj)
+
+    def save(self):
+        """save changes
+        """
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete an element in the table
+        """
+        if obj:
+            self.__session.delete(obj)
+
+    def reload(self):
+        """configuration
+        """
+        Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sec)
+        self.__session = Session()
+
+    def close(self):
+        """Close all working sessions."""
+        self.__session.close()
