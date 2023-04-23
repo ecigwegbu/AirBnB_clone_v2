3c94db2d7c76fac1ffbbbec148f663e8581a8a1a
ecigwegbu <igwegbu@gmail.com>
2023-04-23 19:45:53 +0100

    0. Prepare your web servers - updated

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # install nginx on a new ubuntu server
 
-sudo su
 apt update
 apt install -y nginx
 service nginx start
@@ -14,7 +13,20 @@ ufw --force enable
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 echo "Hello World!" > /data/web_static/releases/test/index.html
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -R ubuntu:ubuntu /data/
-service nginx reload
-exit
+
+# add lines as required
+line1="\tserver_name _;"
+line2="        location \/hbnb_static\/ \{"
+line3="                alias \/data\/web_static\/current\/;"
+line4="        \}"
+sed -i --follow-symlinks "s/^\s*server_name _;/$line1\n$line2\n$line3\n$line4/" \
+        /etc/nginx/sites-enabled/default
