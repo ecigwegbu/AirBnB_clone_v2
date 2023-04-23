d6956f1a21d59ab3670ab5eec7414dd86571372c
ecigwegbu <igwegbu@gmail.com>
2023-04-23 21:30:26 +0100

    0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -30,5 +30,5 @@ line4="        \}"
 sed -i --follow-symlinks "s/^\s*server_name _;/$line1\n$line2\n$line3\n$line4/" \
         /etc/nginx/sites-enabled/default
 
-# reload nginx
+# reload nginx ??
 service nginx restart
